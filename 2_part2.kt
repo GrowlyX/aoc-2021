9766f4d8de07503cc694992070f9b185f2dec7a3
Subham <growlyxdev@gmail.com>
2021-12-02 08:38:15 -0500

    [kotlin/2] add solution to part 2

---
+++ 2_part2.kt
@@ -0,0 +1,35 @@
+import java.io.File
+import java.util.*
+
+val numberTriple = MutableTriple(0, 0, 0)
+
+val mutators = mutableMapOf<String, (Int) -> Unit>(
+	"forward" to { 
+		numberTriple.first += it 
+		numberTriple.second += numberTriple.third * it
+	},
+	"down" to { numberTriple.third += it },
+	"up" to { numberTriple.third -= it }
+)
+
+fun main(args: Array<String>) 
+{
+  val scanner = Scanner(
+	File("input.txt")
+  )
+
+  while (scanner.hasNextLine())
+  {
+	val split = scanner.nextLine().split(' ')
+	
+	mutators[split[0]]?.invoke(split[1].toInt())
+  }
+
+  println("The answer is: ${numberTriple.first * numberTriple.second}")
+}
+
+class MutableTriple<T>(
+	var first: T,
+	var second: T,
+	var third: T
+)
